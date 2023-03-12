2d86bb8502506cd8ec4f28b02779968d9457973a
Arun Patro <arun.kumar.patro@gmail.com>
2023-03-11 21:37:07 -0500

    update

--- scheduler/rust/src/main.rs
+++ scheduler/rust/src/main.rs
@@ -1,3 +1,7 @@
+struct Process {
+    
+}
+
 fn main() {
     println!("Hello, world!");
 }
--- scheduler/rust/src/schedulers.rs
+++ scheduler/rust/src/schedulers.rs
@@ -1,5 +1,25 @@
+use std::collections::VecDeque;
+
 struct Scheduler {
     name: String,
     quantum: u64,
     runQ: VecDeque<Process>,
+}
+
+impl Scheduler {
+    fn new(name: String) -> Self {
+        Scheduler {
+            name,
+            quantum: 1e4,
+            runQ: VecDeque::new(),
+        }
+    }
+
+    fn add_process(&mut self, process: Process) {
+        self.runQ.push_back(process);
+    }
+
+    fn get_next_process(&mut self) -> Option<Process> {
+        self.runQ.pop_front()
+    }
 }
