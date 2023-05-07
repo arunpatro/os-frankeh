d2c44fbe5bf425da5fa775b0fd410ee3d5b887a9
arunpatro <arun.kumar.patro@gmail.com>
2023-05-07 00:21:11 -0400

    add changes

---
+++ iosched/src/ioshed.cpp
@@ -0,0 +1,26 @@
+// imports
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <cassert>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+    int c;
+    while ((c = getopt(argc, argv, "f:a:o:")) != -1) {
+        switch (c) {
+            case 'f':
+                break;
+            case 'a':
+                break;
+            case 'o':
+                break;
+        }
+    }
+}
